4a6246bc7a7fb35ca6016242f2dc6cfb7bb5eefc
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-09 13:25:47 +0700

    set up jwks

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
 import { parseArgs } from "util";
 import * as jose from "jose";
 
@@ -38,10 +38,28 @@ const kid = options.kid || "default";
 const privateKeyPath = `.data/private-keys/${kid}.key.json`;
 const publicKeyPath = `.data/private-keys/${kid}.pub.json`;
 
-if (!existsSync(publicKeyPath)) {
-  const pair = await jose.generateKeyPair("RS256", { extractable: true });
-  const privateKey = await jose.exportJWK(pair.privateKey);
-  const publicKey = await jose.exportJWK(pair.publicKey);
-  writeFileSync(privateKeyPath, JSON.stringify(privateKey));
-  writeFileSync(publicKeyPath, JSON.stringify(publicKey));
+const publicKey = await (async () => {
+  if (!existsSync(publicKeyPath)) {
+    const pair = await jose.generateKeyPair("RS256", { extractable: true });
+    const privateKey = await jose.exportJWK(pair.privateKey);
+    const publicKey = await jose.exportJWK(pair.publicKey);
+    writeFileSync(privateKeyPath, JSON.stringify(privateKey));
+    writeFileSync(publicKeyPath, JSON.stringify(publicKey));
+    return publicKey;
+  }
+  return JSON.parse(readFileSync(publicKeyPath, "utf8"));
+})();
+
+// Update jwks.json
+const jwks = (() => {
+  try {
+    return JSON.parse(readFileSync(".well-known/jwks.json"));
+  } catch {
+    return { keys: [] };
+  }
+})();
+
+if (!jwks.keys.find((key) => key.kid === kid)) {
+  jwks.keys.push({ ...publicKey, kid });
+  writeFileSync(".well-known/jwks.json", JSON.stringify(jwks, null, 2));
 }
